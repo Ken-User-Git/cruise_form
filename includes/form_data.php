e0bdca1345bc42619218b38399b4f69f06a12e0e
Ken-User-Git <wwevstna911@gmail.com>
2022-12-01 18:54:01 -0500

    Updated

--- includes/form_data.php
+++ includes/form_data.php
@@ -2,6 +2,9 @@
 
 // This is our data layer which will provide data to the form
 
+// Use the data layer -- we're in the same directory, so no need to add "includes/"
+require_once('data_layer.php');
+
 /** 
  * Return an array of States
  * 
@@ -9,18 +12,17 @@
  */
 function getStates() {
     // Create an array of states for the dropdown
-    $statesArray = array(
-        "CT" => "Connecticut",
-        "RI" => "Rhode Island",
-        "VT" => "Vermont",
-        "MA" => "Massachusetts",
-        "NH" => "New Hampshire"
-    );
 
-    // Sort the array
-    asort($statesArray);
+    // The form will still get an array as it expected, but we'll pull the data from the database instead of creating it ourselves
+    $results = queryDatabase("SELECT * FROM state ORDER BY state_name"); // Get all states and sort by name
 
-    return $statesArray;
+    $statesArray = array(); // Create an empty placeholder
+   
+    while($row = $results->fetch_assoc()) { // Loop over the results from the query
+        $statesArray[$row['state_id']] = $row['state_name']; // Build the new array
+    }
+
+    return $statesArray; // Return the array to the form
 } 
 
 /** 
@@ -30,31 +32,35 @@ function getStates() {
  */
 function getDestinations() {
     // Create an array of destinations for the dropdown
-    $destinationsArray = array(
-        "Bahamas" => "Bahamas",
-        "Barbados" => "Barbados",
-        "Cozumel" => "Cozumel",
-        "Hawaii" => "Hawaii",
-        "Jamaica" => "Jamaica",
-        "Aruba" => "Aruba",
-        "Fiji" => "Fiji"
-    );
 
-    // Sort the array
-    asort($destinationsArray);
+   // The form will still get an array as it expected, but we'll pull the data from the database instead of creating it ourselves
+   $results = queryDatabase("SELECT * FROM destination ORDER BY destination_name");
+   
+   $destinationsArray = array(); // Create an empty placeholder
+  
+   while($row = $results->fetch_assoc()) { // Loop over the results from the query
+       $destinationsArray[$row['destination_id']] = $row['destination_name']; // Build the new array
+   }
 
-    return $destinationsArray;
+   return $destinationsArray; // Return the array to the form
 }
 
-function getCruiseLine() {
-    $cruiseArray = array(
-        "Disney Cruise Line" => "Disney Cruise Line",
-        "Royal Caribbean International" => "Royal Caribbean International",
-        "Carnival Cruise Line" => "Carnival Cruise Line",
-        "Costa Cruises" => "Costa Cruises",
-        "MSC Cruises" => "MSC Cruises"
-        
-    );
-    asort($cruiseArray);
-    return $cruiseArray;
+/** 
+ * Return an array of preferred cruise lines
+ * 
+ * @return array Array of cruise lines
+ */
+function getCruiseLines() {
+    // Create an array of cruise lines
+
+   // The form will still get an array as it expected, but we'll pull the data from the database instead of creating it ourselves
+   $results = queryDatabase("SELECT * FROM cruiseline ORDER BY cruiseline_name");
+   
+   $cruiseLineArray = array(); // Create an empty placeholder
+  
+   while($row = $results->fetch_assoc()) { // Loop over the results from the query
+       $cruiseLineArray[$row['cruiseline_id']] = $row['cruiseline_name']; // Build the new array
+   }
+
+   return $cruiseLineArray; // Return the array to the form
 }
